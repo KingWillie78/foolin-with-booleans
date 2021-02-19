09c710cf7e2b288835b48ba61a4f6778703a1d8a
“Willie” <willie.williams@codeimmersives.com>
2021-02-19 14:12:30 -0500

    almost finished

--- main.js
+++ main.js
@@ -2,7 +2,53 @@
  * YOUR CODE BELOW! *
  ********************/
 
+function moreThan5(num) {
+  return num > 5;
+}
 
+function isNewTopScore(score, topScore) {
+  return score > topScore;
+}
+
+function isInDanger(grade) {
+  return 60 <= grade && grade <= 71;
+}
+
+function isCoasting(grade) {
+  return 72 <= grade && grade <= 83;
+}
+
+function isSucceeding(grade) {
+  return 84 <= grade && grade <= 92;
+}
+
+function isFailing(grade) {
+  return 60 > grade;
+}
+
+function isAcing(grade) {
+  return grade > 92;
+}
+
+function isStudent(role) {
+  return role === 'student';
+}
+
+function isTeacher(role){
+  return role === 'teacher';
+}
+
+function isAdmin(role){
+  return role === 'admin';
+}
+
+function isElementary(level){
+  return level === 'elementary';
+}
+
+function areDifferentPeople(name1, name2){
+  return name1 !== name2;
+}
 
 
 
